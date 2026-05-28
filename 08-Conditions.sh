49da55e15791b09146ec76008fb4ed1fc2f2655e
kiran <batthulakiran@hotmail.com>
2026-05-28 19:08:55 +0530

    hello world

--- 08-Conditions.sh
+++ 08-Conditions.sh
@@ -13,7 +13,7 @@ if [$NUMBER -gt 20]; then
     echo "Given  number $NUMBER is greather to 20"
 
     elif [ "$NUMBER -eq 20"]: then
-
+    echo " Giver bumber is $NUMBER equal to 20"
     else
     echo "Given number $NUMBER is less than 20"
     fi

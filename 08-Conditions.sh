cd85ff2979ba7546a0fa188d0f49df20eea1303f
kiran <batthulakiran@hotmail.com>
2026-05-28 19:01:05 +0530

    hello world

---
+++ 08-Conditions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Number=$1
+
+#gt-greater than
+#lt- less than
+#eq- equal 
+#ne-not equal
+#ge- greter than or equal
+#le-less than or equal
+
+if [$Number -gt 20]; then
+    echo    "Given  number $NUMBERis greather to 20"
+
+    elif [ $Number - 20]
+    else 
+        echo "Given number $NUMBER is less than 20"
+    fi

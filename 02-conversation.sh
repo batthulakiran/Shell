f21411e3d440514413bd0d76b0815e06188592ab
kiran <batthulakiran@hotmail.com>
2026-05-28 16:46:03 +0530

    hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,7 +2,7 @@
 PERSON1=Kiran
 PERSON2=Varam
 
-echo "$PERSON1: Hey Iran , we are going blast you !"
+echo "$PERSON1: Hey $PERSON2 , we are going blast you !"
 echo "$PERSON2: Yes, Pleae, your are welcome !"
-echo "$PERSON1:Hey Iran i am serious "
-echo "$Person2: Hey trumo we are also serious"
+echo "$PERSON1:Hey $PERSON2 i am serious "
+echo "$Person2: Hey $PERSON1 we are also serious"

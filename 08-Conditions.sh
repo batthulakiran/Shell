ce517bfedfb425baeaac6deb20ccc6ef3cf79305
kiran <batthulakiran@hotmail.com>
2026-05-28 19:04:01 +0530

    hello world

--- 08-Conditions.sh
+++ 08-Conditions.sh
@@ -2,17 +2,17 @@
 
 Number=$1
 
-#gt-greater than
-#lt- less than
-#eq- equal 
-#ne-not equal
-#ge- greter than or equal
-#le-less than or equal
+# gt-greater than
+# lt- less than
+# eq- equal 
+# ne-not equal
+# ge- greter than or equal
+# le-less than or equal
 
 if [$Number -gt 20]; then
     echo "Given  number $NUMBERis greather to 20"
 
-    elif [ $Number - 20]
+    elif [ "$Number - 20"]
     else
-        echo "Given number $NUMBER is less than 20"
+    echo "Given number $NUMBER is less than 20"
     fi

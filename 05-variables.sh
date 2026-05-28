c340bd56a2ed534b089afa00c8aa1d618ee80541
kiran <batthulakiran@hotmail.com>
2026-05-28 17:04:58 +0530

    hello world

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,8 +2,7 @@
 Timestamp=$(date)
 echo "Time is $Timestamp"
 Start_time=$(date +%s)
-sleep 10
+sleep 5
 end_time=$(date +%s)
 Total_time= $(($Start_time-$end_time))
-
-echo " Execution time is $total_tiem"
+echo " Execution time is $Total_time"

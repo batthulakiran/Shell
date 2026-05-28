1e8d388440fb4e59c369a171eeccbe9df3b8c844
kiran <batthulakiran@hotmail.com>
2026-05-28 19:15:14 +0530

    hello world

--- 08-Conditions.sh
+++ 08-Conditions.sh
@@ -9,10 +9,9 @@ NUMBER=$1
 # ge- greter than or equal
 # le-less than or equal
 
-if [$NUMBER -gt 20]; then
+if [ $NUMBER -gt 20 ]; then
     echo "Given  number $NUMBER is greather to 20"
-
-    elif [ "$NUMBER -eq 20"]: then
+    elif [ "$NUMBER -eq 20"]; then
     echo " Giver bumber is $NUMBER equal to 20"
     else
     echo "Given number $NUMBER is less than 20"

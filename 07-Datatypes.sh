3916537f668a3a7d3e5b5c0bc661b10b99d2dcf2
kiran <batthulakiran@hotmail.com>
2026-05-28 17:36:27 +0530

    hello world

--- 07-Datatypes.sh
+++ 07-Datatypes.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-NUM1=$1
-NUM2=$2
+NUM1=10
+NUM2=SQL
 
 sum=$(($NUM1+$NUM2))
 
 echo    "sum is : $sum"
+
+#Array
+MOVIES=("kiran","KK","KKKK")
+echo    " movies are : ${MOVIES[@]}"
+echo    "firstmovie: ${Movies[1]}"  

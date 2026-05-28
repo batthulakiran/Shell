bd488e46f27cd4713e156d37f0238b5f9b00477e
kiran <batthulakiran@hotmail.com>
2026-05-28 16:55:53 +0530

    hello world

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ echo "Please enter your name"
+ read USER_NAME # here user-name is variable 
+ echo "username is $USER_NAME"
+ echo "pls enter your password"
+ read -s password 
+
+ echo "password is $password"

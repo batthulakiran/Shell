c22ca98dd6c20f203791514eee6527575432403b
kiran <batthulakiran@hotmail.com>
2026-05-28 16:38:32 +0530

    hello world

---
+++ o2-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bah
+
+echo "Trump: Hey Iran , we are going blast you !"
+echo "Iran: Yes, Pleae, your are welcome !"
+echo "Trump:Hey Iran i am serious "
+echo "Iran: Hey trumo we are also serious"

460e005b398f81e490c776bba97e0a9773daa43a
kiran <batthulakiran@hotmail.com>
2026-05-28 17:18:15 +0530

    hello world

--- 06-Specialvariables.sh
+++ 06-Specialvariables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "All valieables passed to script : $@"
-echo "Number of variables assed : $#"
-echo "First varibale passed:  $1"
-echo "Script name:  $0"
-echo who is running : $USER"
+echo    "All valieables passed to script : $@"
+echo    "Number of variables assed : $#"
+echo    "First varibale passed:  $1"
+echo    "Script name:  $0"
+echo    "who is running : $USER"

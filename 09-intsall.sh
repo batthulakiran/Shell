9052d6547577e2840d3607a5bdad4bebbf82ccb9
kiran <batthulakiran@hotmail.com>
2026-05-31 01:04:49 +0530

    hello world

--- 09-intsall.sh
+++ 09-intsall.sh
@@ -4,19 +4,19 @@ USERID=$(id -u)
 
 #check root access or not
 if [$USERID -ne 0 ]; then 
-echo    "Pls run this script with root access"
-exit 1
+    echo    "Pls run this script with root access"
+    exit 1
 fi
    
-    #echo " i am continuing..."
+    #echo "I am continuing..."
 
     echo    "installing MYSQL..."
     dnf install mysql -y
 
-if [ $? -ne 0]; then 
+    if [$? -ne 0]; then 
     echo "installing MYSQL is ... Failed"
     exit 1
-else 
+    else 
         echo "installin MYSQL is Sucess.."
 
 fi

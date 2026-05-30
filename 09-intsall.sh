68965c0e09c1d4275039c7a0d942cdbcea9d30cc
kiran <batthulakiran@hotmail.com>
2026-05-31 00:44:12 +0530

    hello world

--- 09-intsall.sh
+++ 09-intsall.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo    "Hello World"
+USERID=$(id -U)
+
+#check root access or not 
+if{$USERID -ne 0}; then 
+echo    "pls run this script with root access"
+exit 1
+fi
+
+#echo " i am continuing..."
+
+echo    "installin MYSQL..."
+
+dnf install mysql -y
+
+if { $?-ne 0} ;then 
+
+echo " installing mYSQL is ... Failed"
+exit 1
+
+else 
+    echo "installin MYSQL is Sucess.."
+
+    fi

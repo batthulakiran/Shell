ad97ca5389d74ff5d5ca4ee6318145f0f3332a69
kiran <batthulakiran@hotmail.com>
2026-05-31 17:59:13 +0530

    hello world

---
+++ 13-colors.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log" 
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#check root access or not
+if [ $USERID -ne 0 ]; then 
+    echo    "Pls run this script with root access"
+    exit 1
+fi
+
+# first org -> What are yu trying install
+# second org--> exit code 
+validate(){  
+    if [ $2 -ne 0   ]; then 
+            echo -e "$TIMESTAMP [Error] installing $1 is ... $R Failed $N" | tee -a $LOGS_FILE
+            exit 1
+            else 
+            echo -e "$TIMESTAMP [info] installing $1 is ... $G Success.." |   tee -a $LOGS_FILE
+    fi
+    }   
+    
+
+for package in $@
+    do
+        echo "Installing $package..."
+        dnf list installed $package &>> $LOGS_FILE
+        if [ $? -ne 0 ]; then
+            dnf install $package -y &>> $LOGS_FILE
+            validate $package $?
+            else
+            echo -e "$TIMESTAMP [INFO] $package already installed ... $Y SKIPPING $N"
+        fi
+    done
+
+
+
+    
+
